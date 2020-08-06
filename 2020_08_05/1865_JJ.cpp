4932e55583645db36548f9604a61a3c00de7bd1e
JungJaeLee-JJ <tjems8637@gmail.com>
2020-08-07 00:50:51 +0900

    JJ #4 submit

--- 2020_08_05/1865_JJ.cpp
+++ 2020_08_05/1865_JJ.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
 #include <cstdio>
 #include <queue>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
 int n,m,w;
-long long int dist[501];
-long long int int_max=10000*500;
+long long int dist[502];
 bool flag=false;
 
 struct edge
@@ -15,53 +14,58 @@ struct edge
     int s;
     int e;
     int c;
+
+    edge(int st,int ed,int cost) : s(st),e(ed),c(cost){};
 };
 
 vector<edge> v;
 
-void solv()
-{
-    for(int i=1;i<=n;i++) dist[i]=int_max;
+void initailizing(){
+    for(int i=1;i<=n;i++) dist[i]=0;
+    v.clear();
+    flag = false;
+}
+
+void solv(){
     dist[1]=0;
     //최소거리 찾기
-    for(int i=1;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
        for(int j=0;j<v.size();j++)
        {
-           if(dist[v[j].s]==int_max) continue;
-           if(dist[v[j].s]+v[j].c<dist[v[j].e]) dist[v[j].e]=dist[v[j].s]+v[j].c;
+
+           if(dist[v[j].s]+v[j].c<dist[v[j].e]) {
+               //사이클찾기
+               if(i==n) flag = true;
+
+               //갱신
+               dist[v[j].e]=dist[v[j].s]+v[j].c;
+           }
        }
     }
-    //사이클 찾기
-    for(int j=0;j<v.size();j++)
-    {
-        if(dist[v[j].s]==int_max) continue;
-        if(dist[v[j].s]+v[j].c<dist[v[j].e]) flag=true;
-    }
-
-    //있다면 종료
-    if(flag)
-    {
-        cout<<"-1";
-        return ;
-    }
-
-    for(int i=2;i<=n;i++) printf("%lld\n",(dist[i]==int_max)?-1:dist[i]);
+    printf("%s\n",flag?"YES":"NO");  
 }
 
 int main()
 {
     int t,a,b,c;
     scanf("%d\n",&t);
-    while (t--)
-    {
-        scanf("%d%d%d",&n,&m,&t);
-        for(int i=0;i<m;i++)
-        {
+    while (t--){
+        initailizing();
+        scanf("%d%d%d",&n,&m,&w);
+        for(int i=0;i<m;i++){
             scanf("%d%d%d",&a,&b,&c);
-            v.push_back({a,b,c});
+            edge tmp1(a,b,c);
+            edge tmp2(b,a,c);
+            v.push_back(tmp1);
+            v.push_back(tmp2);
+        }
+        for(int i=0;i<w;i++){
+            scanf("%d%d%d",&a,&b,&c);
+            edge tmp3(a,b,-c);
+            v.push_back(tmp3);
         }
         solv();
-    
+    }
     return 0;
 }
