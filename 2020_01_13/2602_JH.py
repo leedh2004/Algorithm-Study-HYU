b90107fca6589ca573e12fd671f33872d0599e45
2016025105 <wogns3141@naver.com>
2021-01-14 16:05:02 +0900

    JH #3 update

--- 2020_01_13/2602_JH.py
+++ 2020_01_13/2602_JH.py
@@ -1,4 +1,4 @@
-#왜 틀렸을까요 ?
+#22퍼에서 시간초과 뜹니당 ㅠ
 
 magic = list(input())
 devil = list(input())
@@ -6,23 +6,22 @@ angel = list(input())
 result = 0
 L = len(magic)
 
-def solution(now_c, idx, next_list, now_list):
+def solution(idx, next_list, now_list):
     global magic, result, L
     if idx == L:
-        if now_c == magic[-1]:
-            result += 1
+        result += 1
         return
     
     for i, c in enumerate(next_list):
         if(c == magic[idx]):
-            solution(c, idx+1, now_list[i+1:], next_list[i+1:])
+            solution(idx+1, now_list[i+1:], next_list[i+1:])
 
 for i in range(0, len(devil), 1):
     if devil[i] == magic[0] :
-        solution(magic[0], 1, angel[i+1:], devil[i+1:])
-        print(i)
+        solution(1, angel[i+1:], devil[i+1:])
+        # print(i)
     if angel[i] == magic[0] :
-        solution(magic[0], 1, devil[i+1:], angel[i+1:])
-        print(i)
+        solution(1, devil[i+1:], angel[i+1:])
+        # print(i)
 
 print(result)
